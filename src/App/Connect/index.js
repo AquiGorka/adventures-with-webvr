e4c19a38fe04352ea178c1710735a5c876c7a050
Gorka <gorka@aquigorka.com>
2017-11-26 11:49:14 +0100

    Add connect render short url flow

--- src/App/Connect/index.js
+++ src/App/Connect/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
+import nsa from '../../nsa-google'
 
 class Connect extends Component {
+
+  state = { link: false }
+
+  async componentDidMount() {
+    const link = await nsa.initiator()
+    this.setState({ link })
+  }
+
   render() {
-    return <div>Connect</div>
+    if (!this.state.link) {
+      return null
+    }
+
+    return <div>
+      <div>{this.state.link}</div>
+    </div>
   }
 }
 
--- src/nsa-google/index.js
+++ src/nsa-google/index.js
@@ -1,8 +1,26 @@
 import { EventEmitter } from 'events'
+import promisify from 'es6-promisify'
 import GoogleURL from 'google-url'
+import nsa from '../nsa'
 
 const googleUrl = new GoogleURL({ key: GOOGLE_APIKEY })
+const shorten = promisify(googleUrl.shorten.bind(googleUrl))
 
-class NSAGoogle extends EventEmitter {}
+class NSAGoogle extends EventEmitter {
+  constructor({ url = document.location.origin } = {}) {
+    super()
+    this.url = url
+    nsa.on('connect', () => this.emit('connect'))
+    nsa.on('close', () => this.emit('close'))
+    nsa.on('connect', data => this.emit('data', data))
+  }
 
-export default new NSAGoogle
+  initiator = async () => {
+    const signal = await nsa.initiator()
+    return await shorten(`${this.url}?signal=${btoa(JSON.stringify(signal))}`)
+  }
+
+  signal = async () => {}
+}
+
+export default new NSAGoogle()
